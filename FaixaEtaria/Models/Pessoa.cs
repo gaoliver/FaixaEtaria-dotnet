9ab279f0eded13993e39175a6ffdf15aa7dce62d
Gabriel Ramos <gabrielramos@loomer.tech>
2021-04-07 22:32:06 -0300

    Commit all files

---
+++ FaixaEtaria/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using FaixaEtaria.Validação;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtaria.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Remote("UsuarioExisteAsync", "Pessoas")]
+        public string Nome { get; set; }
+
+        [Adultos]
+        public int Idade { get; set; }
+    }
+}
---
+++ FaixaEtaria/Validação/AdultosAtributo.cs
@@ -0,0 +1,21 @@
+﻿using FaixaEtaria.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtaria.Validação
+{
+    public class AdultosAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Pessoa pessoa = (Pessoa)validationContext.ObjectInstance;
+
+            if (pessoa.Idade < 18)
+                return new ValidationResult("Apenas adultos podem se cadastrar");
+            return ValidationResult.Success;
+        }
+    }
+}
